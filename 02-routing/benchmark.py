56686b1bae120b860356205ab28c601c2de5be16
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-10-08 21:09:01 +0300

    Added routing benchmark for wheezy.web.

---
+++ 02-routing/benchmark.py
@@ -0,0 +1,61 @@
+
+"""
+"""
+
+import os
+import profile
+import sys
+
+from pstats import Stats
+from timeit import timeit
+
+from samples import environ
+
+
+if sys.version_info[0] >= 3:
+    b = b''
+else:
+    b = ''
+
+path = os.getcwd()
+
+frameworks = ['wheezy.web']
+
+def start_response(status, headers):
+    return None
+
+
+def run(name, wrapper, number=100000):
+    sys.path[0] = '.'
+    print("\n%-11s   msec    rps  tcalls  funcs" % name)
+    for framework in frameworks:
+        os.chdir(os.path.join(path, framework))
+        try:
+            main = __import__('app', None, None, ['main']).main
+            wrapper(main)
+            time = timeit(lambda: wrapper(main), number=number)
+            st = Stats(profile.Profile().runctx(
+                'wrapper(main)', globals(), locals()))
+            print("%-11s %6.0f %6.0f %7d %6d" % (framework, 1000 * time,
+                number / time, st.total_calls, len(st.stats)))
+            del sys.modules['app']
+        except ImportError:
+            print("%-15s not installed" % framework)
+
+
+def build_wrapper(path_info):
+    def wrapper(main):
+        e = environ.copy()
+        e['PATH_INFO'] = e['REQUEST_URI'] = path_info
+        b.join(main(e, start_response))
+    return wrapper
+
+
+if __name__ == '__main__':
+    run('static[0]', build_wrapper('/section0/feature0'))
+    run('static[-1]', build_wrapper('/section9/feature19'))
+    run('dynamic[0]', build_wrapper('/jsmith/dotfiles/feature0'))
+    run('dynamic[-1]', build_wrapper('/jsmith/dotfiles/feature19'))
+    run('seo[0]', build_wrapper('/en/feature0'))
+    run('seo[-1]', build_wrapper('/ru/feature9'))
+    run('missing', build_wrapper('/not-found'))
---
+++ 02-routing/wheezy.web/app.py
@@ -0,0 +1,37 @@
+import warnings
+
+from wheezy.http import HTTPResponse
+from wheezy.http import WSGIApplication
+from wheezy.routing import url
+from wheezy.web.handlers import BaseHandler
+from wheezy.web.middleware import bootstrap_defaults
+from wheezy.web.middleware import path_routing_middleware_factory
+
+from samples import features
+from samples import sections
+
+
+class WelcomeHandler(BaseHandler):
+
+    def get(self):
+        response = HTTPResponse()
+        response.write('Hello World!')
+        return response
+
+seo_urls = [(f, WelcomeHandler) for f in features]
+dynamic_urls = [(f, WelcomeHandler) for f in features]
+
+all_urls = []
+all_urls += [url('%s/%s' % (s, f), WelcomeHandler)
+        for s in sections for f in features]
+all_urls += [url('{locale:(en|ru)}/', seo_urls)]
+all_urls += [url('{user}/{repo}/', dynamic_urls)]
+
+warnings.simplefilter('ignore')
+main = WSGIApplication(
+    middleware=[
+        bootstrap_defaults(url_mapping=all_urls),
+        path_routing_middleware_factory
+    ],
+    options={}
+)
