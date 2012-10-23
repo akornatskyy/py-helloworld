2b99e933767f3a445441614a673da419fbcbc614
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-10-23 12:26:05 +0300

    Applied html escape to template benchmark.

--- 05-template/tornado/app.py
+++ 05-template/tornado/app.py
@@ -6,6 +6,6 @@ from tornado.template import Loader
 
 
 def main(name):
-    loader = Loader(root_directory=name)
+    loader = Loader(root_directory=name, autoescape=None)
     template = loader.load('welcome.html')
     return lambda ctx: template.generate(**ctx)
