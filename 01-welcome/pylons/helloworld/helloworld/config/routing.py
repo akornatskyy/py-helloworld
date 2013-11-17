4c5c791e5960ebe774ffa9fc329d388d4aa05ec0
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2013-11-17 11:50:30 +0100

    Disabled error and default controller mappings.

--- 01-welcome/pylons/helloworld/helloworld/config/routing.py
+++ 01-welcome/pylons/helloworld/helloworld/config/routing.py
@@ -15,12 +15,13 @@ def make_map(config):
 
     # The ErrorController route (handles 404/500 error pages); it should
     # likely stay at the top, ensuring it can always be resolved
-    map.connect('/error/{action}', controller='error')
-    map.connect('/error/{action}/{id}', controller='error')
+    #map.connect('/error/{action}', controller='error')
+    #map.connect('/error/{action}/{id}', controller='error')
 
     # CUSTOM ROUTES HERE
 
-    map.connect('/{controller}/{action}')
-    map.connect('/{controller}/{action}/{id}')
+    #map.connect('/{controller}/{action}')
+    #map.connect('/{controller}/{action}/{id}')
+    map.connect('/{controller}', action='index')
 
     return map
